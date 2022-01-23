15984a3e870f168ef1280320cad8f40b9cdbdec8
Hrabovenskyi-Yaroslav <grabovenskyi@gmail.com>
2022-01-23 20:09:07 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,11 +1,14 @@
 
 const bodyRef = document.querySelector('body')
 const changeColorRef = document.querySelector('.change-color')
+const colorRef = document.querySelector('.color');
+
 
 changeColorRef.addEventListener('click', onButtonClick);
 
 function onButtonClick() {
   bodyRef.setAttribute('style', `background-color: ${getRandomHexColor()};`);
+  colorRef.textContent = bodyRef.style;
 }
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
